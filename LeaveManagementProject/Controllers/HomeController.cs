04c51710d6cdf1fb82a9379ba7f053e97e7a7a6d
sindhu g <gundalasrisindhu@gmail.com>
2020-07-24 17:58:45 -0400

    Add project files.

---
+++ LeaveManagementProject/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using LeaveManagementProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LeaveManagementProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        LMSEntities lmsentities = new LMSEntities();
+     
+        public ActionResult Index()
+        {
+            return View("_Login");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signin(Signin login)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                var user = lmsentities.tblLogins.SingleOrDefault(x => (x.UserName == login.UserName) & (x.Password == login.Password));
+                if (user != null)
+                {
+                    ViewBag.Message = "Login Successful";
+                    return View("_Signin");
+                }
+            }
+            return View("Error");
+
+        }
+    }
+}
---
+++ LeaveManagementProject/Models/Signin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagementProject.Models
+{
+    public class Signin
+    {
+        [Required(ErrorMessage ="Please enter the UserName")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Please enter Passwor")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+
+    }
+}
